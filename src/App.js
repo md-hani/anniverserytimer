7c3641ee68d1a90176d8889b9584a70226e3f60b
md-hani <hani13may@yahoo.com>
2024-06-14 00:00:56 -0500

    Project finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const now = new Date();
+  const hour = (now.getHours()).toString().padStart(2, "0");
+  const min = now.getMinutes().toString().padStart(2, "0");
+  const sec = now.getSeconds().toString().padStart(2, "0");
+  const msc = now.getMilliseconds().toString();
+
+  const [swtime, setSwtime] = useState(0);
+
+  setInterval(() => {
+    setSwtime(swtime + 1000);
+  }, 1000);
+
+  if (hour === "23" && min === "59" & sec === "59" & msc === "50") {
+    var elements = document.getElementsByClassName('myText');
+    console.log(elements);
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      element.classList.toggle('visible');
+      document.body.classList.toggle("add-background");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className='myText'>Happy 5 Months Anniversary Darling!!!!</p>
+      <div className='container'>
+        <div className='clock-container'>
+          <p>{hour[0]}</p>
+        </div>
+        <div className='clock-container'>
+        <p>{hour[1]}</p>
+        </div>
+        <div className='separators'>
+          <div className='separator1'></div>
+          <div className='separator2'></div>
+        </div>
+        <div className='clock-container'>
+          <p>{min[0]}</p>
+        </div>
+        <div className='clock-container'>
+          <p>{min[1]}</p>
+        </div>
+        <div className='separators'>
+          <div className='separator1'></div>
+          <div className='separator2'></div>
+        </div>
+        <div className='clock-container'>
+          <p>{sec[0]}</p>
+        </div>
+        <div className='clock-container'>
+          <p>{sec[1]}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
